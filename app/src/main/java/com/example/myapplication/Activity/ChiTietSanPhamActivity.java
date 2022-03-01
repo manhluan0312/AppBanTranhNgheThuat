4cf59abd65efb9ca60118033f5ef4e920783946a
luan <manhluan0312@gmail.com>
2022-03-01 17:57:45 +0700

    tao du an

---
+++ app/src/main/java/com/example/myapplication/Activity/ChiTietSanPhamActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class ChiTietSanPhamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_san_pham);
+
+    }
+}
